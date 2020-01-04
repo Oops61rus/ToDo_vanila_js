d9677baf99eb9378813876db8c6df701d1673c77
Oops61rus <antodeni@rambler.ru>
2020-01-04 22:52:46 +0300

    add function

--- js/app.js
+++ js/app.js
@@ -1,89 +1,76 @@
-// var allTask = [];
-
-// var field = document.querySelector('#inputField');
-// var todoList = document.querySelector('#todoList');
-// var newTodo = document.querySelector('#newTodo');
-// var todoList = document.querySelector('#pagingContainer');
-
-// function Task(value) {
-//     this.id = new Date().getTime();
-//     this.isComplete = false;
-//     this.value = value;
-// }
-
-// function getValueFromInput() {
-//     var valueFromInput = field.value;
-//     field.value = "";
-//     return new Task(valueFromInput);
-// }
-
-// function createTextTask(task) {
-//     var newTask = document.createElement('li');
-//     newTask.innerHTML = task.value;
-//     return newTask;
-// }
-
-// function createButton(text) {
-//     var newButton = document.createElement('button');
-//     newButton.innerHTML = text;
-//     return newButton;
-// }
-
-// function createTask(task) {
-//     var container = document.createElement('div');
-//     container.classList.add('container')
-//     container.append(createButton('delete'));
-//     container.append(createTextTask(task));
-//     container.append(createButton('complete'));
-//     return container;
-// }
-
-// function render() {
-//     // while (todoList.lastChild) {
-//     //     todoList.removeChild(todoList.lastChild);
-//     // }
-//     var task = getValueFromInput();
-//     allTask.push(task);
-//     allTask.forEach(function(item) {
-//         var todo = createTask(item);
-//         todoList.append(todo);
-//     });
-// }
-
-// newTodo.addEventListener('click', render);
-
-
-
 var field = document.querySelector('#inputField');
 var todoList = document.querySelector('#todoList');
 var newTodo = document.querySelector('#newTodo');
 
+function App() {
+    this.allTask = [];
+}
+    
+App.prototype.addTask = function(value) {
+    const task = new Task(value);
+    console.log(task);
+    this.allTask.push(task);
+}
+
+var appNew = new App();
+
+function Task(value) {
+    this.id = new Date().getTime();
+    this.isComplete = false;
+    this.value = value;
+}
+
 function getTextTask() {
     var todo = field.value;
-    todoList.append(todo);
+    field.value = "";
     return todo;
+}
+
+function makeTask() {
+    const text = getTextTask();
+    if (text) {
+        appNew.addTask(text);
+        render();
+    }
+}
+
+function render() {
+    while (todoList.lastChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+    var fragment = document.createDocumentFragment();
+    appNew.allTask.forEach(function(task) {
+        var rowTask = createTask(task);
+        fragment.append(rowTask);
+    });
+    todoList.append(fragment);
 }
 
 function createBtn(text) {
     var btn = document.createElement('button');
     btn.append(text);
+    btn.classList.add(text);
     return btn;
 }
 
-function createTask() {
-    var div = document.createElement('div');
-    var listItem = document.createElement('li');
-
-    div.append(createBtn('Complete'));
-    div.append(getTextTask());
-    div.append(createBtn('Delete'));
-
+function createTask(task) {
+    var listItem = document.createElement('li');    
+    var text = document.createTextNode(task.value);
+    var span = document.createElement('span');
+    span.append(text);
+    listItem.append(createBtn('Complete'));
+    listItem.append(span);
+    listItem.append(createBtn('Delete'));
     listItem.classList.add('container');
-    listItem.append(div);  
-    
-    console.log(listItem); 
-
-    todoList.append(listItem);
+    listItem.classList.add(task.isCompleted ? 'is__completed' : 'non__completed');
+    return listItem;
 }
 
-newTodo.addEventListener('click', createTask);     
+newTodo.addEventListener('click', makeTask);     
+
+var completeBtn = document.querySelector('.Complete');
+var nonComplete = document.querySelector('.non__Complete');
+
+function isComplete() {
+
+}
